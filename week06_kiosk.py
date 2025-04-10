cf25a47a7649e492152b75330a7c57aabc42ef13
taeho0n <hoontae0522@naver.com>
2025-04-10 11:44:35 +0900

    v1.3

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,11 +1,14 @@
 # 1) 아아 : 2000 2) 라떼 : 2500
-drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스"]
-prices = [1500, 2500, 4000]
-amounts = [0, 0, 0]
+drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스","딸기 주스"]
+prices = [1500, 2500, 4000, 4200]
 # drinks = ["아이스 아메리카노"]
 # prices = [1500]
 # amounts = [0]
 total_price = 0
+
+amounts = list()
+for _ in range (len(drinks)):
+    amounts.append(0)
 
 def order_process(idx):
     """
