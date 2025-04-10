e8837c7739770dbe47bd0210e519579d068b2c32
taeho0n <hoontae0522@naver.com>
2025-04-10 10:06:34 +0900

    5week

---
+++ week05.py
@@ -0,0 +1,28 @@
+def is_prime(number : int) -> bool:
+
+    """
+    Function to determine whether a number is a prime number
+    :param number:The number to determine whether it is prime or not
+    :return: Returns True if it is prime number, False if it is a prime number
+    """
+
+   # is_prime = True
+
+    if number >= 2:
+        for i in range(2,int(number**0.5)+1):
+            if number % i ==0:
+                return False
+    else:
+        return False
+    return  True
+
+n1= int(input())
+n2= int(input())
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+
+for i in range(n1,n2+1):
+    if is_prime(i):
+        print(i, end=' ')
