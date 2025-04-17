8f02f4ce3e4b0db20c505c67a53f013a6d28caef
taeho0n <hoontae0522@naver.com>
2025-04-17 12:01:22 +0900

    v2.0

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -33,15 +33,20 @@ def print_receipt()->None:
 
     print(f"총 주문 금액 : {total_price}원")
 
+# menu=0 while문 전체를 try문에 넣었을 때 첫번째 해결방법
+
 while True:
-    menu = int(input(display_menu()))
-    if len(drinks)>= menu >= 1:
-       order_process(menu-1)
-    elif menu == len(drinks)+1:
-        print("주문을 종료합니다")
-        break
-    else:
-        print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
+    try:
+        menu = int(input(display_menu()))
+        if len(drinks)>= menu >= 1:
+           order_process(menu-1)
+        elif menu == len(drinks)+1:
+            print("주문을 종료합니다")
+            break
+        else:
+            print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
+    except ValueError:
+        print(f"문자를 입력할 수 없습니다. 숫자를 입력해주세요")
 
 print_receipt()
 
